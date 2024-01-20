d1f03f85c0f5811fabf46bda2503f485321a2acc
Diovane Alves <alvdiovane@gmail.com>
2024-01-20 14:18:45 -0300

    Initial commit: Setup Project using Vue

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import LoginViewVue from '@/views/LoginView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginViewVue
+    }
+  ]
+})
+
+export default router
