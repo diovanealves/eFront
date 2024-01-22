dc659ebc877f0a569cbc374921e1e9677ff807b2
Diovane Alves <alvdiovane@gmail.com>
2024-01-21 23:36:36 -0300

    feat: Add icon dependency and configure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
 
+import PhosphorIcons from '@phosphor-icons/vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +8,5 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
-
+app.use(PhosphorIcons)
 app.mount('#app')
