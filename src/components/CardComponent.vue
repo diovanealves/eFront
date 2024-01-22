2e0973e025ddd0738a77fa72f341c9cee90fd7d4
Diovane Alves <alvdiovane@gmail.com>
2024-01-21 23:37:51 -0300

    feat: Create CardComponent and CardData for rendering data

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const { icon, title, description } = defineProps(['icon', 'title', 'description'])
+</script>
+
+<template>
+  <div>
+    <component :is="icon" class="text-4xl" />
+    <h1 class="text-2xl font-medium mt-7 mb-4">{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
---
+++ src/components/CardData.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+import { PhClock, PhDiscordLogo, PhLightning, PhTarget } from '@phosphor-icons/vue'
+import { ref } from 'vue'
+import CardComponent from './CardComponent.vue'
+
+const cardDataList = ref([
+  {
+    icon: PhClock,
+    title: 'Reembolso',
+    description:
+      'Se por algum motivo você não gostar dos materiais você pode reembolsar sem taxa e perguntas.'
+  },
+  {
+    icon: PhLightning,
+    title: 'Vitalício',
+    description:
+      'Você tem acesso a todas as atualizações e para sempre. Basta acessar a plataforma da Hotmart.'
+  },
+  {
+    icon: PhDiscordLogo,
+    title: 'Comunidade',
+    description:
+      'Você terá acesso a comunidade exclusiva do Discord, onde utilizamos para resolver dúvidas.'
+  },
+  {
+    icon: PhTarget,
+    title: 'Notion',
+    description:
+      'Através do Notion você tem acesso aos materiais para realizar os estudos sobre frontend e design.'
+  }
+])
+
+export default {
+  components: { CardComponent },
+  setup() {
+    return { cardDataList }
+  }
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-14 mt-16 mx-auto">
+    <CardComponent
+      v-for="data in cardDataList"
+      :key="data.title"
+      :icon="data.icon"
+      :title="data.title"
+      :description="data.description"
+    />
+  </div>
+</template>
